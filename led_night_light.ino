1b8d3f6b88a50fd17fa6187e70dcd73b129f93ac
Aleksey Profatilov <snakeduse@gmail.com>
2016-11-03 23:49:08 +0300

    Почистил код

--- led_night_light.ino
+++ led_night_light.ino
@@ -1,19 +1,21 @@
 ///
-/// Пины датчиков
+/// Скетч ночника в спальню, который включается по датчику движения
 ///
+
+// Пины датчиков
 #define RED_PIN 11
 #define GREEN_PIN 10
 #define BLUE_PIN 9
 #define PIR_PIN 8
-#define LIGHT_PIN 7
 
 // время горения света - 5 мин.
 #define TIME_DELAY 300000
 
-// Включен ли свет
+// включен ли свет
 int _isLight;
 
 void setup() {
+
   // led лента
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
@@ -21,12 +23,6 @@ void setup() {
 
   // датчик движения
   pinMode(PIR_PIN, INPUT);
-
-  // датчик света
-  pinMode(LIGHT_PIN, INPUT);
-
-  //Serial.begin(9600);
-  //Serial.println("Controller is init");
 
   _isLight = 0;
 }
@@ -36,30 +32,17 @@ void loop() {
   // значение датчика движения
   int pirValue = digitalRead(PIR_PIN);
 
-  //Serial.print("Pir value: ");
-  //Serial.println(pirValue);
-
-  //Serial.print("_isLight = ");
-  //Serial.println(_isLight);
-
-  int lightSensor = digitalRead(LIGHT_PIN);
-  //Serial.print("Light sensor: ");
-  //Serial.println(lightSensor);
   delay(10);
 
-  // если lightSensor == 1 - света нет
-  if(pirValue == HIGH && _isLight == 0/* && lightSensor == 1*/){
+  if(pirValue == HIGH && _isLight == 0){
     _isLight = 1;
-    //Serial.println("Set color");
 
     setColor(90, 90, 90);
 
     delay(TIME_DELAY);
 
-    //Serial.println("Reset light");
     _isLight = 0;
     setColor(0, 0, 0);
-    //Serial.println();
   }
 }
 
